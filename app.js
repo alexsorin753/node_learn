cd9f1fca5e8f4e11e8af8b3c1c2027713ef1aadc
alexsorin753 <alex.songureanu74@gmail.com>
2021-11-18 14:28:47 +0200

    update

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1'
-const port = 3000
+// const hostname = '127.0.0.1'
+// const port = 3000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World\n')
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.end('Hello World\n')
+// });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`)
-});
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}`)
+// });
 
 
-// exit program after 10 seconds
+// // exit program after 10 seconds
+// setTimeout(() => {
+//     process.exitCode = 1;
+//     process.exit(1)
+// }, 10000);
+
+
+
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(3000, () => console.log('Server ready - http://localhost:3000/'));
+
 setTimeout(() => {
-    process.exitCode = 1;
-    process.exit(1)
-}, 10000);
+    console.log(process.env.USER_ID)
+    console.log(process.env.USER_KEY)
+    process.kill(process.pid, 'SIGTERM');
+}, 1000);
+
