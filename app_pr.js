41aa5b7bf5630e4b626365f78378f497c9a40727
alexsorin753 <alex.songureanu74@gmail.com>
2021-12-02 14:28:49 +0200

    adding pug to production

--- app_pr.js
+++ app_pr.js
@@ -1,12 +1,18 @@
 // https://nodejs.dev/learn/an-introduction-to-the-npm-package-manager#running-tasks
-const {port, host} = require('../config_env');
+const {port, host} = require('./config_env');
 
 const express = require('express');
 const app = express();
 const chalk = require('chalk');
+const path = require('path');
+const pug = require('pug');
+
+// view engine setup
+app.set('../views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('test', { pageTitle: 'Express' });
 });
 
 app.listen(port, () => console.log(`${chalk.green('Server ready')} - http://${host}:${port}/`));
